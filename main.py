4797598ee21d75bb64def03b8ece2b80f372acf5
youk77799 <63965544+youk77799@users.noreply.github.com>
2022-01-25 19:52:00 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+coordinates = (4, 5)
+# variable = [] Lists
+# variable = () tuples
+print(coordinates[0])
+# you gonna get error if you trying to change tuples
+# Ex. coordinates[1] = 10
+Target = [(4, 5), (6, 7), (80, 34)]
+print(Target)
